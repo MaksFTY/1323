687cff40401bce7fe606dc6917b44a0737437d2b
maxsi <maxsim.polyanko@gmail.com>
2022-11-27 09:23:00 +0200

    Initial commit

---
+++ ff.py
@@ -0,0 +1,64 @@
+import random
+
+class Human:
+    def __init__(self, name='Human', job=None, home=None, car=None):
+        self.name = name
+        self.job = job
+        self.home = home
+        self.car = car
+        self.money = 100
+        self.satiety = 75
+        self.gladness = 50
+        self.day = 1
+        self.mood = 49
+
+
+class is_alive:
+    def __init__(self, mood, satietry, money):
+        self.mood = random
+        self.satietry = if_Human_eat_satiety_level_rises
+        self.money = 100
+
+    if indicators > 0 and money = -300
+    return False
+
+
+class Auto:
+    def __init__(self, brand_list):
+        self.brand = random.choice(list(brand_list))
+        self.fuel = brand_of_car[self.brand]['fuel']
+        self.strength = brand_of_car[self.brand]['strength']
+        self.consumption = brand_of_car[self.brand]['consumption']
+        self.petrol = 75
+        self.condition = 60
+
+    def drive(self):
+            self.cif self.strength > 0 and self.fuel onsumption:
+            self.fuel -= self.consumption
+            self.strength -= 1
+            return True
+        esle:
+            print('The car cannot move')
+            return False
+
+    class Home:
+        def __init__(self):
+            self.mess = 0
+            self.food = (if go_to_shop_foot = foot 50)
+            self.go_to_shop_foot = 50
+
+class Job:
+    def __init__(self, job_list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[salf.job]['salary']
+        self
+
+job_list = {'Java': {'salary': 50, 'gladness_less': 10},
+            'Python': {'salary': 40, 'gladness_less': 3},
+            'C++': {'salary': 70, 'gladness_less': 25},
+            'Rust': {'salary': 60, 'gladness_less': 1}}
+
+brand_of_car = {'BMW': {'fuel': 100, 'strength': 100, 'consumption': 6},
+                'Lada': {'fuel': 50, 'strength': 40, 'consumption': 10},
+                'Volvo': {'fuel': 70, 'strength': 150, 'consumption': 8},
+                'Ferrari': {'fuel': 80, 'strength': 120, 'consumption': 14}}
---
+++ main.py
@@ -0,0 +1,122 @@
+# class Human:
+#     height = 170
+#     satiety = 50
+#
+#
+# class Student(Human):
+#     satiety = 0
+#
+#
+# class Worker(Human):
+#     # satiety = 100
+#     pass
+#
+#
+# nick = Student()
+# anna = Worker()
+# print(nick.height)
+# print(anna.height)
+# print(nick.satiety)
+# print(anna.satiety)
+# class Grandparent:
+#     height = 170
+#     satiety = 100
+#     age = 70
+#
+#
+# class Parent(Grandparent):
+#     # age = 45
+#     pass
+# class Children(Parent):
+#     height = 50
+#     def __init__(self):
+#         print(self.height)
+#         print(self.satiety)
+#         print(self.age)
+#
+# nick = Children()
+
+# class Hello_world:
+#     hello = 'hello'
+#     _hello = '_hello'
+#     __hello = '__hello'
+#     def __init__(self):
+#         self.world = 'hello'
+#         self._world = '_hello'
+#         self.__world = '__hello'
+#     def printer(self):
+#         print(self.hello)
+#         print(self._hello)
+#         print(self.__hello)
+#         print(self.world)
+#         print(self._world)
+#         print(self.__world)
+#
+# class Hi(Hello_world):
+#     def hi_printer(self):
+#         print(self.hello)
+#         print(self.world)
+#         print(self._hello)
+#         print(self._world)
+#         # print(self.__hello)
+#         # print(self.__world)
+#         # print(self._Hello_world__hello)
+#         # print(self._Hello_world__world)
+#
+# hello = Hello_world()
+# hello.printer()
+# hi = Hi()
+# hi.hi_printer()
+
+# class Hello:
+#     def __init__(self):
+#         print("Hello")
+#     def parens(self):
+#         print('Parens')
+#
+# class Hello_world(Hello):
+#     def __init__(self):
+#         # print("World")
+#         self.name = 'name'
+#         super().__init__()
+#
+#     def parens(self):
+#         print('Children')
+#         # super().__init__()
+#
+#
+# hello_world = Hello_world()
+# hello_world.parens()
+
+
+# class Class1:
+#     var = 20
+#     def __init__(self):
+#         self.var = 10
+#
+# class Class2(Class1):
+#     def __init__(self):
+#         print(self.var)
+#         super().__init__()
+#         print(self.var)
+#         print(super().var)
+#
+#
+# cl = Class2()
+
+class Grandparent:
+    def about(self):
+        print('I am Grandparent')
+    def about_myself(self):
+        print('I am Grandparent')
+class Parent(Grandparent):
+    def about_myself(self):
+        print('I am Parent')
+class Children(Parent):
+    height = 50
+    def __init__(self):
+        print(self.height)
+        print(self.satiety)
+        print(self.age)
+
+nick = Children()
